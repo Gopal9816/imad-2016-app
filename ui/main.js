a73c4ed49691ca0da119cd466cb56ee99e12431a
Gopal9816 <gopalkurup@gmail.com>
2016-09-28 14:57:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById("text");
 element.onclick = function (){
 element.innerHTML = "This is the text entered in main.js";
-}
+};
 //Making the image move around
 var img = document.getElementById("madi");
 img.onclick = function (){
