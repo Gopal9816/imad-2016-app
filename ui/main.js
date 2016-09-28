decad042e0bea0d15b3913972a1b80113c125684
Gopal9816 <gopalkurup@gmail.com>
2016-09-28 14:47:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //changing the main text
-var element = document.getELementById("text");
+var element = document.getElementById("text");
 element.innerHTML = "This  is the text entered in main.js";
