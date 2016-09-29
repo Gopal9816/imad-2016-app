e9fa6d4a6285a9594b6e52a2d02275ddace84c13
Gopal9816 <gopalkurup@gmail.com>
2016-09-29 20:15:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var counter;
+var counter=0;
 app.get('/counter',function(req,res){
     counter = counter+1;
     res.send(counter.toString());
